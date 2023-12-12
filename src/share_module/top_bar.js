2349ccc8888a5223ff38644fc19e2ba0c7f314de
narasimha <narasimha.m@thrymr.net>
2023-12-12 10:37:24 +0530

    create the top bar

--- src/share_module/top_bar.js
+++ src/share_module/top_bar.js
@@ -1,5 +1,24 @@
 export const TopBar = () => {
   return (
-    <nav class="fixed top-0 w-full flex flex-wrap items-center justify-between py-3 bg-cockpit_secondary-950 4xl:!px-24 xl:px-16 4xl:!h-48 xl:h-20 text-cockpit_secondary-200 shadow-lg navbar navbar-expand-lg navbar-light"></nav>
+    <nav className="fixed top-0 w-full flex flex-wrap items-center justify-between py-3 bg-secondary-950 4xl:!px-24 xl:px-16 4xl:!h-48 xl:h-20 text-secondary-200 shadow-lg navbar navbar-expand-lg navbar-light">
+      <div className="container-fluid w-full flex flex-wrap items-center justify-between">
+        <div className="flex-grow items-center">
+          <p>Logo</p>
+        </div>
+        <div class="flex items-center">
+          <div className="dropdown relative">
+            <a
+              class="dropdown-toggle flex items-center hidden-arrow"
+              id="dropdownMenuButton2"
+              role="button"
+            >
+              <div class="flex justify-center items-center 4xl:!h-20 xl:h-10 4xl:!w-20 xl:w-10 rounded-full text-white bg-[#A929E5]">
+                <p class="4xl:!text-4xl xl:text-xl">{"N"}</p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 };
